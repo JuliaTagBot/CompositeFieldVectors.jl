92a7f34624560e272e573bd4d3817bcab0e4220f
Rafael Schouten <rafaelschouten@gmail.com>
2018-05-05 08:47:26 +1000

    basic working composed fieldvector

--- src/ParameterComposition.jl
+++ src/ParameterComposition.jl
@@ -1,5 +1,41 @@
 module ParameterComposition
 
-# package code goes here
+using StaticArrays
+
+
+import Base: getindex, setindex!, size, similar, vec, show, length, convert, promote_op,
+             promote_rule, map, map!, reduce, reducedim, mapreducedim, mapreduce, broadcast,
+             broadcast!, conj, hcat, vcat, ones, zeros, one, reshape, fill, fill!, inv,
+             iszero, sum, prod, count, any, all, minimum, maximum, extrema, mean,
+             copy, read, read!, write
+
+export CompositeFieldVector, 
+       getindex, 
+       setindex!, 
+       size, 
+       length
+
+const TUPLELOOKUP = 3
+abstract type CompositeFieldVector{N,T,L} end
+
+length(x::CompositeFieldVector) = length(typeof(x).parameters[TUPLELOOKUP])
+size(x::CompositeFieldVector) = (length(x),)
+
+getindex(v::CompositeFieldVector, i::Int) = 
+    dogetfield(v, typeof(v).parameters[TUPLELOOKUP], i)
+
+setindex!(v::CompositeFieldVector, x, i::Int) = begin
+    dosetfield!(v, typeof(v).parameters[TUPLELOOKUP], i, x)
+    nothing
+end
+
+dosetfield!(v, l, i, x) = recursive_setfield!(v, l[i], x) # Separated out for type stability
+dogetfield(v, l, i) = recursive_getfield(v, l[i]) # Separated out for type stability
+
+recursive_setfield!(v, l::Tuple{Int,Vararg}, x) = recursive_setfield!(getfield(v, l[1]), l[2:end], x); nothing
+recursive_setfield!(v::V, l::Tuple{Int}, x) where V = setfield!(v, l[1], x); nothing
+
+recursive_getfield(v, l::Tuple{Int,Vararg}) = recursive_getfield(getfield(v, l[1]), l[2:end])
+recursive_getfield(v, l::Tuple{Int}) = getfield(v, l[1])
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,36 @@
+using Revise
 using ParameterComposition
+
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+mutable struct SubParams
+    p3::Float64 # 2
+    p4::Float64 # 3
+end
+
+mutable struct Params{N,T,L} <: CompositeFieldVector{N,T,L}
+    p1::T          # 1
+    sub::SubParams # 2, 3
+    p2::T          # 4
+end
+
+subparams = SubParams(2.0, 3.0)
+params = Params{4,Float64,((1,),(2,1),(2,2),(3,))}(1.0, subparams, 4.0)
+
+@test params[1] == 1.0
+@test params[2] == 2.0
+@test params[3] == 3.0
+@test params[4] == 4.0
+
+@test length(params) == 4
+@test size(params) == (4,)
+params[1] = 11.0
+@test params[1] == 11.0
+setindex!(params, 33.0, 3)
+@code_warntype setindex!(params, 33.0, 3)
+@code_warntype getindex(params, 3)
+@test params[3] == 33.0
